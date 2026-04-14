5664ecd8f7d7e8e42330f7eda5a7f0d2abf28fb2
Rizky Ahmad Fahrezi <raf281005@gmail.com>
2026-04-15 00:06:26 +0700

    Mengedit Bagian Partial Sidebar

--- resources/views/partials/sidebar.blade.php
+++ resources/views/partials/sidebar.blade.php
@@ -2,11 +2,19 @@
     <link rel="stylesheet" href="{{ asset('css/component/sidebar.css') }}">
 </head>
 <aside class="sidebar">
-    <ul>
-        <li>barang</li>
-        <li>transaksi</li>
-        <li>laporan</li>
-        <li>User Management</li>
-        <li>Pengaturan</li>
+    <img src="{{ asset('images/Logo Sinar Jaya Inventory.png') }}" alt="logo">
+    <ul class="menu">
+        <li><a href="">Dashboard</a></li>
+        <li><a href="">Barang</a></li>
+        <li><a href="">Transaksi</a></li>
+        <li><a href="">Laporan</a></li>
+        <li><a href="">User Management</a></li>
+    </ul>
+    <ul class="setting">
+        <li><a href="">Settings</a></li>
+        <form action="{{ route('logout') }}" method="post">
+            @csrf
+            <button type="submit">Logout</button>
+        </form>
     </ul>
 </aside>
--- routes/web.php
+++ routes/web.php
@@ -6,9 +6,11 @@ use Illuminate\Support\Facades\Route;
 
 Route::get('/', [AuthController::class, 'index']);
 Route::post('Login', [AuthController::class, 'Login'])->name('Login');
+Route::post('logout',[AuthController::class,'logout'])->name('logout');
 
 // Route validationn Role
 Route::get('/Admin-Dashboard', [AdminController::class, 'index'])
     ->middleware('auth')
     ->name('Admin-Dashboard');
 Route::get('/User-Dashboard', [AdminController::class, 'index'])->name('User-Dashboard')->middleware('auth');
+
